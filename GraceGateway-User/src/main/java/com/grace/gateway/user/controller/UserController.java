becd57909c6a60af4db8b5ab4a973c25990a2ebf
soyorin <3495746536@qq.com>
2025-09-02 21:52:24 +0800

    update 完善配置文件

--- GraceGateway-User/src/main/java/com/grace/gateway/user/controller/UserController.java
+++ GraceGateway-User/src/main/java/com/grace/gateway/user/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.grace.gateway.user.controller;
 
 
+import java.util.concurrent.atomic.LongAdder;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 public class UserController {
-
+    LongAdder longAdder = new LongAdder();
     @GetMapping("/api/user/ping1")
     public String ping1() {
+        log.info("请求-"+longAdder.intValue());
+        longAdder.increment();
         return "this is user ping1";
     }
 
